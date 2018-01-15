e22d8eff68ed5fea512a412cb49deb6b936c40ad
amandama <amandama.999@gmail.com>
2018-01-15 15:27:12 -0500

    Implemented Butterknife and Glide

--- app/src/main/java/io/intrepid/thirdpartylibraryexcercise/MainActivity.java
+++ app/src/main/java/io/intrepid/thirdpartylibraryexcercise/MainActivity.java
@@ -2,6 +2,8 @@ package io.intrepid.thirdpartylibraryexcercise;
 
 import android.app.ProgressDialog;
 import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
+import android.media.Image;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -9,29 +11,32 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity implements MainContract.View, DownloadCatImageTask.Callback {
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 
-    ImageView imageView;
-    Button getNewCatButton;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity implements MainContract.View {
 
     private MainPresenter presenter;
     private ProgressDialog dialog;
+
+    @BindView(R.id.image_view)
+    ImageView imageView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
         presenter = new MainPresenter();
         presenter.bindView(this);
-
-        imageView = (ImageView) findViewById(R.id.image_view);
-        getNewCatButton = (Button) findViewById(R.id.new_cat_button);
-        getNewCatButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                onNewCatButtonClick();
-            }
-        });
     }
 
     @Override
@@ -40,6 +45,7 @@ public class MainActivity extends AppCompatActivity implements MainContract.View
         presenter.bindView(this);
     }
 
+    @OnClick(R.id.new_cat_button)
     public void onNewCatButtonClick() {
         presenter.onNewCatButtonClick();
     }
@@ -62,15 +68,24 @@ public class MainActivity extends AppCompatActivity implements MainContract.View
 
     @Override
     public void loadImageFromUrl(String url) {
-        DownloadCatImageTask downloadCatImageTask = new DownloadCatImageTask();
-        downloadCatImageTask.setCallback(this);
-        downloadCatImageTask.execute(url);
-    }
+        Glide.with(this)
+                .load(url)
+                .listener(new RequestListener<Drawable>() {
+                    @Override
+                    public boolean onLoadFailed(@Nullable GlideException e, Object model,
+                                                Target<Drawable> target, boolean isFirstResource) {
+                        dismissLoadingIndicator();
+                        return false;
+                    }
 
-    @Override
-    public void onImageResult(Bitmap bitmap) {
-        dismissLoadingIndicator();
-        imageView.setImageBitmap(bitmap);
+                    @Override
+                    public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target,
+                                                   DataSource dataSource, boolean isFirstResource) {
+                        dismissLoadingIndicator();
+                        return false;
+                    }
+                })
+                .into(imageView);
     }
 
     @Override
